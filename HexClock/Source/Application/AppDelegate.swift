d5eb12b986566f2fd04dd35d771be53d23a17b9e
Colin Drake <colin.f.drake@gmail.com>
2017-05-12 19:07:30 -0500

    Avoid showing screensaver when running app

--- HexClock/Source/Application/AppDelegate.swift
+++ HexClock/Source/Application/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UIApplicationDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Avoid showing the screensaver while running this app.
+        application.isIdleTimerDisabled = true
+
         window = UIWindow()
         window?.rootViewController = HexClockViewController()
         window?.makeKeyAndVisible()
